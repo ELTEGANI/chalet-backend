1117f6985c6a3d3c93b627bf5882d2286242344a
altegani mohammed <tiganimoh@gmail.com>
2020-07-07 23:21:10 +0200

    adds forget password

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,6 +10,9 @@ router.post('/booking',isAuth,userController.userReservation);
 router.put('/updatefirebasetoken',isAuth,userController.updateUserFireBaseToken);
 router.post('/userlogin',userController.userLogin);
 router.put('/userverifcation',isAuth,userController.verifyUserCode);
+router.post('/forgetpassword',userController.resetPassword);
+router.post('/verifypasswordchange',userController.verifyResetPassword);
+router.post('/updatepassword',userController.updatePassword);
 
 
 module.exports = router;
